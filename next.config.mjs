42f234394880a88b6d40d4f978a8a55097b2578e
Daniel <145457665+Daniel-Vitor-Duarte@users.noreply.github.com>
2024-04-22 16:48:24 -0300

    att url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: "/te-peguei-curioso",
+    trailingSlash: true,
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+  },
+};
 
 export default nextConfig;
